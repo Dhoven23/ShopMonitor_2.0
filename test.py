a95e5f0e1423528118740ed25109c260b0ac29be
Dhoven <dhoven@my.gcu.edu>
2020-07-17 17:15:26 -0700

    face list module

--- test.py
+++ test.py
@@ -1,33 +1,20 @@
-from tkinter import Button, Tk, DISABLED
+import cv2
+import time
+import numpy as np
 
-root = Tk()
+StudentID = 20481553
 
-
-
-
-
-class ToolButton:
-    button = Button()
-    def __init__(self, master, x, y):
-        def Onclick():
-            print(number)
-        number = x * y
-        self.button = Button(master, text=str(number), width=20, command=Onclick)
-        self.button.grid(column=x, row=y)
-        print(self.button)
-
-
-
-
-
-
+screen = np.zeros((1080,1920,3))
 
 def main():
-    for x in range(1, 5):
-        for y in range(1, 5):
-            ToolButton.__init__(ToolButton,root, x, y)
+    face = cv2.imread(f"{StudentID}.jpg")
+
+    screen[0:640,0:480] = face
+    cv2.imshow('test', face)
+    cv2.imshow('blank', screen)
+    cv2.waitKey()
+    print(np.shape(face))
 
 
 if __name__ == '__main__':
     main()
-    root.mainloop()
