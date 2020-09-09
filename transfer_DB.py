1508cf19c40d30add908a147947cba75c277219b
Dhoven23 <dhoven@my.gcu.edu>
2020-09-09 10:56:22 -0700

    Delete transfer_DB.py

--- transfer_DB.py
+++
@@ -1,9 +0,0 @@
-from Data.mongo_setup import global_init,disconnect_all
-from mongoengine import connect
-global_init("DHoven","12345")
-
-DB_URI = f"mongodb+srv://DHoven:12345@cluster0-lbs9s.mongodb.net/beta1?retryWrites=true&w=majority"
-connect(host=DB_URI,alias='core2')
-
-disconnect_all()
-
