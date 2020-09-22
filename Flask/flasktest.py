b45c121098c0799176547d4e9ebcd27c049810b5
Daniel Hoven <dhoven@my.gcu.edu>
2020-09-21 17:56:26 -0700

    monday commit = add flask app

--- Flask/flasktest.py
+++ Flask/flasktest.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from Data.mongo_setup import global_init
+from Data import Students, key
 import Service.admin_svc as asv
 global_init('DHoven','12345')
 
@@ -11,11 +12,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    messages = str()
-    message = asv.whos_in_the_shop()
-    for mess in message:
-        messages = messages + mess + '<br>'
-    return f"<h1>{messages}</h1>"
+    students = Students.Student.objects(Is_signedIn = True)
+    keys = key.Key.objects()
+    return render_template('home.html', students=students, keys = keys)
 
 
 @app.route("/about")
